a2f91c49125a0ce670f3f3b02c4e034d4be20251
Punsisi99 <punsisichamodha99@gmail.com>
2024-06-26 09:58:59 +0530

    modify the project page

--- script.js
+++ script.js
@@ -16,3 +16,11 @@ menuIcon.addEventListener("click",()=>{
     menuIcon.classList.toggle("active");
 });
 
+//counting of portfolio
+const projectCount = document.querySelector(".portfolio-container .portfolio-box");
+const volunteeringCount = document.getElementById("volunteering");
+const certificatesCount = document.getElementById("certificates");
+
+function countProject(){
+    let projects = portfolio-container.value;
+}
